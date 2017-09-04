bb46806c8d32659ae7bfce8596eac34c5b0ca75a
SlyAtBest <slyatbest@gmail.com>
2017-09-04 18:06:28 +0100

    Add json output.

--- cukes-framework/src/test/java/com/github/slyatbest/cukes_framework/CukesRunTest.java
+++ cukes-framework/src/test/java/com/github/slyatbest/cukes_framework/CukesRunTest.java
@@ -7,8 +7,8 @@ import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", tags = {"~@ignore"}, plugin = {"pretty"},
-        snippets = SnippetType.CAMELCASE, strict = true)
+@CucumberOptions(features = "src/test/resources", tags = {"~@ignore"},
+        plugin = {"pretty", "json:target/cucumber-report.json"}, snippets = SnippetType.CAMELCASE, strict = true)
 public class CukesRunTest
 {
 
