b38f673e3c73bed7c99b8cac9713f148e777f834
MatD <mat.durrant@clearswift.com>
2017-12-15 11:04:23 +0000

    Fixed up Java problems, removed unnecessary @override annotations, added missing noop comment and removed the runOnce code for adding the POST_bulk command to the start of the output file.

--- cukes-framework/src/test/java/com/github/slyatbest/cukes_framework/elasticsearch/ElasticFormatter.java
+++ cukes-framework/src/test/java/com/github/slyatbest/cukes_framework/elasticsearch/ElasticFormatter.java
@@ -30,67 +30,52 @@ public class ElasticFormatter implements Reporter, Formatter
     private Feature feature;
     private int beforeHook = STARTING_BEFORE_HOOK_VALUE;
     private int afterHook = STARTING_AFTER_HOOK_VALUE;
-    private boolean runOnce = true;
 
     public ElasticFormatter(Appendable out)
     {
         this.out = new NiceAppendable(out);
     }
 
-    @Override
     public void syntaxError(String state, String event, List<String> legalEvents, String uri, Integer line)
     {
         // Noop
     }
 
-    @Override
     public void uri(String uri)
     {
         // Noop
     }
 
-    @Override
     public void feature(Feature feature)
     {
         this.feature = feature;
     }
 
-    @Override
     public void scenarioOutline(ScenarioOutline scenarioOutline)
     {
         // Noop
     }
 
-    @Override
     public void examples(Examples examples)
     {
         // Noop
     }
 
-    @Override
     public void startOfScenarioLifeCycle(Scenario scenario)
     {
-        // At the top of the file add the bulk post command
-        if (runOnce)
-        {
-            out.append("POST _bulk\n");
-            runOnce = false;
-        }
+        // Noop
     }
 
-    @Override
     public void background(Background background)
     {
         // Noop
     }
 
-    @Override
     public void scenario(Scenario scenario)
     {
         // Noop
     }
 
-    @Override
     public void step(Step step)
     {
         // Check if the step is an example step (can't use instanceof because it's private)
@@ -109,7 +94,6 @@ public class ElasticFormatter implements Reporter, Formatter
         steps.add(step);
     }
 
-    @Override
     public void endOfScenarioLifeCycle(Scenario scenario)
     {
         // Check that all lists are the same size, if they are not something has gone horribly wrong
@@ -136,25 +120,21 @@ public class ElasticFormatter implements Reporter, Formatter
         afterHook = STARTING_AFTER_HOOK_VALUE;
     }
 
-    @Override
     public void done()
     {
-
+        // Noop
     }
 
-    @Override
     public void close()
     {
         out.close();
     }
 
-    @Override
     public void eof()
     {
         // Noop
     }
 
-    @Override
     public void before(Match match, Result result)
     {
         steps.add(new Step(null, "Before", match.getLocation(), beforeHook++, null, null));
@@ -162,13 +142,11 @@ public class ElasticFormatter implements Reporter, Formatter
         matches.add(match);
     }
 
-    @Override
     public void result(Result result)
     {
         results.add(result);
     }
 
-    @Override
     public void after(Match match, Result result)
     {
         steps.add(new Step(null, "After", match.getLocation(), afterHook++, null, null));
@@ -176,19 +154,16 @@ public class ElasticFormatter implements Reporter, Formatter
         matches.add(match);
     }
 
-    @Override
     public void match(Match match)
     {
         matches.add(match);
     }
 
-    @Override
     public void embedding(String mimeType, byte[] data)
     {
         // Noop
     }
 
-    @Override
     public void write(String text)
     {
         // Noop
