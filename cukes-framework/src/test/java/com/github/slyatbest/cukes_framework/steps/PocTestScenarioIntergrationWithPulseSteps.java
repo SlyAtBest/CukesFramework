367a06e8eb029ffd8e2766b741b6047d62815345
slyatbest <matd@clearswift.com>
2019-01-31 13:53:23 +0000

    Implemented a step to see if the pulse color action will successfully update Jira

---
+++ cukes-framework/src/test/java/com/github/slyatbest/cukes_framework/steps/PocTestScenarioIntergrationWithPulseSteps.java
@@ -0,0 +1,12 @@
+package com.github.slyatbest.cukes_framework.steps;
+
+import cucumber.api.java.en.Given;
+
+public class PocTestScenarioIntergrationWithPulseSteps
+{
+    @Given("^this step passes$")
+    public void thisStepPasses() throws Exception
+    {
+        // Do nothing
+    }
+}
